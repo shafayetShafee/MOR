f137ffe8f160f73eda3565f6e1df00aed59ffca8
shafayetShafee <sshafee@isrt.ac.bd>
2023-09-08 20:16:53 +0600

    added mor method for glmmTMB pkg

--- R/mor.R
+++ R/mor.R
@@ -1,4 +1,4 @@
-#' Calculate Median Odds Ratio from a fitted multilevel binary logistic model object
+#' Calculate Median Odds Ratio from a fitted multilevel binary logistic model object.
 #'
 #' @param object An `MixMod` object created by `GLMMadaptive::mixed_model()` or
 #'   an `glmmTMB` object created by `glmmTMB::glmmTMB()`.
@@ -62,14 +62,14 @@ mor.MixMod <- function(object, se = TRUE, conf.int = TRUE, conf.level = 0.95, ..
   log_se_mor_hat <- as.numeric(sqrt(t(J) %*% var_sigma_u_sq_hat %*% J))
   se_mor_hat <- exp(log_se_mor_hat)
 
-  crit_val <- stats::qnorm((1 - conf.level)/2, lower.tail = F)
+  crit_val <- qnorm((1 - conf.level)/2, lower.tail = F)
   ci <- log_mor_hat + c(-1, 1) * crit_val * log_se_mor_hat
   ci_exp <- exp(ci)
   ci_lower <- ci_exp[1]
   ci_upper <- ci_exp[2]
 
   if(!se) {
-    se = NULL
+    se_mor_hat = NULL
   }
 
   if(!conf.int) {
@@ -81,6 +81,141 @@ mor.MixMod <- function(object, se = TRUE, conf.int = TRUE, conf.level = 0.95, ..
     mor = mor_hat, se_mor = se_mor_hat, ci_lower = ci_lower, ci_upper = ci_upper
   ))
 }
+
+#' @importFrom stats qnorm vcov
+#' @export
+mor.glmmTMB <- function(object, se = TRUE, conf.int = TRUE, conf.level = 0.95, ...) {
+
+  obj_family <- object$modelInfo$family
+  if(obj_family$family != "binomial" && obj_family$link != "logit") {
+    stop("MOR can only be calculated for Multilevel Binary Logistic Regression Model i.e. for `MixMod` with `binomial` family with `logit` link",
+         call. = FALSE)
+  }
+
+  par_vcov <- diag(vcov(object, full=TRUE))
+  par_names <- names(par_vcov)
+  grp_var_names <- object$modelInfo$grpVar
+  grp_var_no <- length(grp_var_names)
+  ran_par_no <- sum(grepl("theta", par_names))
+
+  pattern <- paste0("theta.*[", paste0(grp_var_names, collapse = "|"), "]")
+  ran_terms_idx <- which(grepl(pattern, par_names))
+  ran_terms_names <- par_names[ran_terms_idx]
+
+  se_logsd <- sqrt(par_vcov)[ran_terms_idx] # sd(log_sd)
+  logsd <- object$sdr$par.fixed[ran_terms_idx] # log_sd
+  sigma_sq_hat <- unname(exp(2*logsd))
+  # by delta method
+  # se(sigma_sq_hat) = sqrt((2 * exp(2*logsd))^2) * se(logsd)
+  se_sigma_sq_hat <- se_logsd*2*exp(2*logsd)
+  var_sigma_sq_hat <- se_sigma_sq_hat^2
+
+  if(grp_var_no == 1) {
+    if(ran_par_no > 1) {
+      stop("MOR can only be calculated for Two level random intercept model.", call. = FALSE)
+    }
+
+    mor_hat <- exp(sqrt(2 * sigma_sq_hat) * qnorm(0.75))
+    log_mor_hat <- log(mor_hat)
+
+    log_mor_expr <- function(x) {
+      # x is ran-effect parameterized as sd
+      sqrt(2 * x) * qnorm(0.75)
+    }
+
+    J <- numDeriv::jacobian(log_mor_expr, x = sigma_sq_hat)
+    log_se_mor_hat <- as.numeric(sqrt(t(J) %*% var_sigma_sq_hat %*% J))
+    se_mor_hat <- exp(log_se_mor_hat)
+
+    crit_val <- stats::qnorm((1 - conf.level)/2, lower.tail = F)
+    ci <- log_mor_hat + c(-1, 1) * crit_val * log_se_mor_hat
+    ci_exp <- exp(ci)
+    ci_lower <- ci_exp[1]
+    ci_upper <- ci_exp[2]
+
+    if(!se) {
+      se_mor_hat = NULL
+    }
+
+    if(!conf.int) {
+      ci_lower = NULL
+      ci_upper = NULL
+    }
+
+    return(c(
+      mor = mor_hat, se_mor = se_mor_hat, ci_lower = ci_lower, ci_upper = ci_upper
+    ))
+
+  } else if(grp_var_no == 2) {
+    if(!ran_par_no == 2) {
+      stop("MOR can only be calculated for Three level random intercept model.", call. = FALSE)
+    }
+
+    fix_terms_names <- names(attr(object$modelInfo$terms$cond$fixed, "dataClasses"))
+    model_terms_names <- names(object$frame)
+    nested_term_names <- paste0(setdiff(model_terms_names, fix_terms_names),
+                                collapse = ":")
+    # third_lvl_var_names <- setdiff(grp_var_names, nested_term_names)
+
+    sigma_ujk_idx <- grepl(nested_term_names, names(se_sigma_sq_hat), fixed = TRUE)
+    sigma_ujk <- sigma_sq_hat[sigma_ujk_idx]
+    mor1_hat <- exp(sqrt(2 * sigma_ujk) * qnorm(0.75))
+    log_mor1_hat <- log(mor1_hat)
+
+    log_mor1_expr <- function(x) {
+      # x is ran-effect parameterized as sd
+      sqrt(2 * x) * qnorm(0.75)
+    }
+
+    J1 <- numDeriv::jacobian(log_mor1_expr, x = sigma_ujk)
+    log_se_mor1_hat <- as.numeric(sqrt(t(J1) %*% var_sigma_sq_hat[sigma_ujk_idx] %*% J1))
+    se_mor1_hat <- exp(log_se_mor1_hat)
+
+    # mor2 calc. ----------------------
+    mor2_hat <- exp(sqrt(2 * sum(sigma_sq_hat)) * qnorm(0.75))
+    log_mor2_hat <- log(mor2_hat)
+
+    log_mor2_expr <- function(x) {
+      # x is ran-effect parameterized as variance
+      sqrt(2 * sum(x)) * qnorm(0.75)
+    }
+
+    J2 <- numDeriv::jacobian(log_mor2_expr, x = sigma_sq_hat)
+    log_se_mor2_hat <- as.numeric(sqrt(J2 %*% diag(var_sigma_sq_hat) %*% t(J2)))
+    se_mor2_hat <- exp(log_se_mor2_hat)
+
+    crit_val <- stats::qnorm((1 - conf.level)/2, lower.tail = F)
+
+    ci_1 <- log_mor1_hat + c(-1, 1) * crit_val * log_se_mor1_hat
+    ci_1_exp <- exp(ci_1)
+    ci_lower_1 <- ci_1_exp[1]
+    ci_upper_1 <- ci_1_exp[2]
+
+    ci_2 <- log_mor2_hat + c(-1, 1) * crit_val * log_se_mor2_hat
+    ci_2_exp <- exp(ci_2)
+    ci_lower_2 <- ci_2_exp[1]
+    ci_upper_2 <- ci_2_exp[2]
+
+    if(!se) {
+      se_mor1_hat = NULL
+      se_mor2_hat = NULL
+    }
+
+    if(!conf.int) {
+      ci_1_lower = NULL
+      ci_1_upper = NULL
+      ci_2_lower = NULL
+      ci_2_upper = NULL
+    }
+
+    return(c(
+      mor_1 = mor1_hat, se_mor_1 = se_mor1_hat, ci_lower_1 = ci_lower_1, ci_upper_1 = ci_upper_1,
+      mor_2 = mor2_hat, se_mor_2 = se_mor2_hat, ci_lower_2 = ci_lower_2, ci_upper_2 = ci_upper_2
+    ))
+
+  }
+}
+
 
 
 # helper funs -------------------------------------------------------------
@@ -106,25 +241,25 @@ chol_transf <- function (x) {
   }
 }
 
-# taken from GLMMadaptive pkg
-vcov.MixMod <- function (object, parm = "var-cov", ...) {
-  parm <- match.arg(parm)
-  V <- solve(object$Hessian)
-  if (parm == "var-cov") {
-    D <- object$D
-    diag_D <- ncol(D) > 1 && all(abs(D[lower.tri(D)]) < sqrt(.Machine$double.eps))
-    include <- if (diag_D) {
-      unconstr_D <- log(diag(D))
-      n_betas <- length(object$coefficients)
-      seq(n_betas + 1, n_betas + length(unconstr_D))
-    } else {
-      unconstr_D <- chol_transf(D)
-      n_betas <- length(object$coefficients)
-      seq(n_betas + 1, n_betas + length(unconstr_D))
-    }
-    return(V[include, include, drop = FALSE])
-  }
-}
+# # taken from GLMMadaptive pkg
+# vcov.MixMod <- function (object, parm = "var-cov", ...) {
+#   parm <- match.arg(parm)
+#   V <- solve(object$Hessian)
+#   if (parm == "var-cov") {
+#     D <- object$D
+#     diag_D <- ncol(D) > 1 && all(abs(D[lower.tri(D)]) < sqrt(.Machine$double.eps))
+#     include <- if (diag_D) {
+#       unconstr_D <- log(diag(D))
+#       n_betas <- length(object$coefficients)
+#       seq(n_betas + 1, n_betas + length(unconstr_D))
+#     } else {
+#       unconstr_D <- chol_transf(D)
+#       n_betas <- length(object$coefficients)
+#       seq(n_betas + 1, n_betas + length(unconstr_D))
+#     }
+#     return(V[include, include, drop = FALSE])
+#   }
+# }
 
 # copied from https://stackoverflow.com/a/76925070/10858321 by @statmerkur
 D_chol_to_D <- function(x) {
@@ -133,10 +268,11 @@ D_chol_to_D <- function(x) {
   D[upper.tri(D, diag = TRUE)]
 }
 
+#' @importFrom stats vcov
 vcov_orig_scale <- function(model) {
   D <- model$D
   D_chol_entries <- chol_transf(D)
-  V_chol <- stats::vcov(model, parm = "var-cov")
+  V_chol <- vcov(model, parm = "var-cov")
   J <- numDeriv::jacobian(D_chol_to_D, D_chol_entries)
   V <- J %*% V_chol %*% t(J)
   colnames(V) <- colnames(V_chol)
